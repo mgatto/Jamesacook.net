55e08ab819fb5c5bc8b5208c6369ba3c4655a563
mgatto <mgatto@5dc27c84-7d59-e345-960d-2867a70b0a43>
2010-09-26 18:52:44 +0000

    * Cleaned up old html file; * Merged old nav.js into main.js to save on HTTP calls.
    git-svn-id: svn://localhost@21 5dc27c84-7d59-e345-960d-2867a70b0a43

--- Trunk/js/main.js
+++ Trunk/js/main.js
@@ -11,27 +11,29 @@ $(document).ready(function() {
 		case "sculpture":
 			uri = "/jamesacook.net/sculpture/index.html";
 			break;
+		case "mediated-sculpture":
+			uri = "/jamesacook.net/mediated-sculpture/index.html";
+			break;
+		case "video":
+			uri = "/jamesacook.net/video/index.html";
+			break;
 		case "spiel":
-			uri = "/jamesacook.net/sculpture/spiel.html";
+			uri = "/jamesacook.net/spiel/index.html";
 			break;
 		default:
 			uri = "";
 	}
 
 	$("ul#navigation li a[href=\""+uri+"\"]").parent().addClass("active");
-	$("ul#navigation li a[href=\""+uri+"\"]").addClass("active_link");   
-
+	$("ul#navigation li a[href=\""+uri+"\"]").addClass("active_link");
+	
+	//Colorbox
 	$('img.art').colorbox({
 		rel:'group', 
 		photo:true, 
 		transition: 'fade',
-		href:function(){ return this.src; }
-	}); 
-	
-	//$('img#nav1').hover(function() {
-	//$(this).attr("src","/images/headerNav/1-over.gif");
-	//	}, function() {
-	//$(this).attr("src","/images/headerNav/1.gif");
+		href: function(){ 
+			return this.src; 
+		}
+	});
 });
-
-//<img src="1.gif" id="nav1" />
