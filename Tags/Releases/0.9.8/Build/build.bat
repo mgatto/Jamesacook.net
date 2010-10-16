e69861694e8dddc3a9c5211b233859af89678573
mgatto <mgatto@5dc27c84-7d59-e345-960d-2867a70b0a43>
2010-10-16 00:28:32 +0000

    * Tagging 0.9.8 for release to Testing/Staging Server.
    git-svn-id: svn://localhost@95 5dc27c84-7d59-e345-960d-2867a70b0a43

---
+++ Tags/Releases/0.9.8/Build/build.bat
@@ -0,0 +1,20 @@
+; relies on TCC's 4NT shell or its successor Take Command LE
+
+; process css
+
+
+
+; process js
+
+
+
+;strip comments from html
+
+
+
+
+
+tidy -config config.txt file1.html file2.html
+
+; convert ' -> &#39; since &apos; is not standard
+ quote-marks:yes
---
+++ Tags/Releases/0.9.8/Build/build2.sh
@@ -0,0 +1,115 @@
+#! /bin/bash -x
+
+# must be run as root
+if [ ! "`whoami`" = "root" ]
+then
+    echo "\nPlease run script as root."
+    exit 1
+fi
+
+echo "Preparing to build."
+echo "Current Directory: $PWD"
+
+read -p "Do you wish to change dir?" YN
+case $YN in
+    [Yy]* ) read -p "To which path?" PATH; chdir $PATH ;;
+    [Nn]* )  ;;
+    * ) echo "Please answer yes or no.";;
+esac
+
+read -p "Which version are we deploying?" VERSION
+
+# does the VERSION dir already exist?
+if [ -d "./$VERSION" ];
+then
+    echo "Version: $VERSION already exists."
+
+    read -p "Delete it?" DELETE
+    case $DELETE in
+        [Yy]* ) rm -rf "./$VERSION" ;;
+        [Nn]* )  ;;
+        * ) echo "Please answer yes or no.";;
+    esac
+fi
+
+# check signature; gpg returns 0 if all is well, 1 for bad sig.
+echo "Checking signature of Jamesacook.net-$VERSION.zip"
+if [ $(gpg --verify Jamesacook.net-$VERSION.zip.sig Jamesacook.net-$VERSION.zip) -ne 0 ];
+then
+  echo "Signature verification failed!"
+  exit 1
+fi
+
+#unzip it; -a converts windows EOL -> UNIX; but, it thinks jpg and flv are text
+unzip "Jamesacook.net-$VERSION.zip" -d ./
+
+# its easier (?) to do further operations inside
+cd "$VERSION"
+
+#delete .svn & _notes dirs
+find . -name ".svn" -type d -exec rm -rf {} \;
+find . -name "_notes" -type d -exec rm -rf {} \;
+
+#find . -name ".svn" -type d | xargs -n1 rm -R
+
+# Delete Templates, Library and Build
+rm -rf ./Templates
+rm -rf ./Library
+rm -rf ./Build
+
+# strip all comments
+#s|<!--[^>]*-->||g
+#or
+#s|<!--(.*?)-->||g
+
+# rewrite links if necessary
+echo "rewriting links, excluding binary files"
+find . -type f \( -name "*.html" -o -name "*.css" -o -name "*.js" \) | xargs sed -i \
+        -e 's|href=\"/|href=\"/jamesacook.net/|g' \
+        -e 's|src=\"/|src=\"/jamesacook.net/|g' \
+        -e "s|url('/|url('/jamesacook.net/|g" \
+        -e "s|url(/|url(/jamesacook.net/|g" \
+        -e "s|uri = \"/|uri = \"/jamesacook.net/|g" \
+        -e "s|src: '|src: '/jamesacook.net/media/|g" \
+        -e "s|url: '|url: '/jamesacook.net/media/|g";
+
+#Erase LibrayItem lines...
+#find . -type f -name "*.html" | xargs sed -i \
+#        -e "s|<!-- #EndLibraryItem -->||g" \
+#        -e "s|<!-- #BeginLibraryItem * -->||g";
+
+# reverse the link rewriting...
+#find . -type f \( -name "*.html" -o -name "*.css" -o -name "*.js" \) | xargs sed -i \
+#        -e 's|href=\"/jamesacook.net/|href=\"/|g' \
+#        -e 's|src=\"/jamesacook.net/|src=\"/|g' \
+#        -e "s|url('/jamesacook.net/|url('/|g" \
+#        -e "s|url(/jamesacook.net/|url(/|g" \
+#        -e "s|uri = \"/jamesacook.net/|uri = \"/|g" \
+#        -e "s|src: '/jamesacook.net/media/|src: '|g" \
+#        -e "s|url: '/jamesacook.net/media/|url: '|g";
+
+# this is one reason this script needs to be run as root
+chown www-data:www-data . -R
+
+cd ..
+
+echo "Symlinking"
+# symlink htdocs
+ln -sfn $VERSION htdocs
+
+echo "Deployed!"
+
+# usage: set_owner user group
+set_owner() {
+    OWNER = $1
+    GROUP = $2
+    chown "{$USER}:{$GROUP}" . -R
+}
+
+symlink() {}
+get_from_svn() {}
+get_from_hg() {}
+get_from_tbz2() {}
+get_from_tar.bz2() {} #alias of get_from_tbz2()
+
+rewrite_links() {}
