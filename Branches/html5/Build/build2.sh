4492819ba95443f1d43ce80659dbe68431023dd6
mgatto <mgatto@5dc27c84-7d59-e345-960d-2867a70b0a43>
2010-10-27 20:40:01 +0000

    MERGED from latest Trunk.
    git-svn-id: svn://localhost@154 5dc27c84-7d59-e345-960d-2867a70b0a43

--- Branches/html5/Build/build2.sh
+++ Branches/html5/Build/build2.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /bin/bash -x
 
 # must be run as root
 if [ ! "`whoami`" = "root" ]; then
@@ -7,17 +7,19 @@ if [ ! "`whoami`" = "root" ]; then
 fi
 
 PROGNAME="$0"
-SHORTOPTS="f:t:c::d:v:"
-LONGOPTS="from:,to:,conf::,repository::,archive::,domain:,version:,rewrite,reverse-rewrite,compress-css::,compress-js::"
+SHORTOPTS="f:t:c::d:v:h"
+LONGOPTS="from:,to:,conf::,repository::,archive::,domain:,version:,rewrite,reverse-rewrite,combine-css::,combine-js::help"
 SVN=0
 ZIP=0
 TO="$PWD"
 CONF="no"
 VERSION=""
+## although I want to use 0.9.9 and such, we must be careful to manually increment
+## this so scripts-0.9.9.min.js is NOT shared between builds IF it changed, else it will be cached which we do NOT want
 ARCHIVE=""
 REPOSITORY=""
 REWRITE="no"
-COMPRESS="no"
+COMBINE="no"
 CSSFILES=""
 JSFILES=""
 
@@ -88,8 +90,8 @@ while true ; do
             REWRITE="REMOVE_DOMAIN"
             shift
             ;;
-        --compress-css)
-            echo "Option --compress-css, argument \`$2'" ;
+        --combine-css)
+            echo "Option --combine-css, argument \`$2'" ;
             case "$2" in
                 "")
                     shift 2
@@ -100,8 +102,8 @@ while true ; do
                     ;;
             esac
             ;;
-        --compress-js)
-            echo "Option --compress-js, argument \`$2'" ;
+        --combine-js)
+            echo "Option --combine-js, argument \`$2'" ;
             case "$2" in
                 "")
                     shift 2
@@ -166,13 +168,19 @@ symlink() {
 
 
 get_from_svn() {
-    svn export \
-        $1 \
-        $TO/$VERSION;
+    # --non-interactive  --native-eol LF
+    BASE=$1
+    TO=$2
+    VERSION=$3
+    svn checkout "file://$BASE/Tags/Releases/$VERSION" $TO/$VERSION;
 
     # Get the videos which are not under VC
     echo "Importing video files"
-    cp ./htdocs/media/video/*.f4v ./$VERSION/media/video/
+    if [ -d ./htdocs/media/video/ ]; then
+        cp ./htdocs/media/video/*.f4v ./$VERSION/media/video/
+    else
+        echo "Videos not found; please copy them manually"
+    fi
 }
 
 get_from_zip() {
@@ -229,17 +237,37 @@ rewrite_links() {
 
 # Remove VCS and Editor Templates and artifacts
 cleanup_files() {
+    # Delete Templates, Library and Build
+    rm -rf ./Templates
+    rm -rf ./Library
+    rm -rf ./Build
+
     #delete .svn Dreamweaver _notes dirs
     find . -name ".svn" -type d -exec rm -rf {} \;
     find . -name ".hg" -type d -exec rm -rf {} \;
     find . -name ".git" -type d -exec rm -rf {} \;
     find . -name ".bzr" -type d -exec rm -rf {} \;
     find . -name "_notes" -type d -exec rm -rf {} \;
+}
 
-    # Delete Templates, Library and Build
-    rm -rf ./Templates
-    rm -rf ./Library
-    rm -rf ./Build
+#make sure everything is UTF-8
+encode() {
+    # this might not be a good idea:
+
+#    ONLY convert file that really are in old charset to new charset
+#    - if you convert a file that was already in the new charset format
+#    or that you converted manually before
+#    or inserted text components in new charset inbetween old text components
+#
+#    --- then you may get something worse ... neither UTF-8 and nor ISO-8859-1 ...
+    #hence make sure your OLD file IS in OLD charset before running the tool !!
+
+
+    find . -type f \( -name "*.html" -o -name "*.css" -o -name "*.js" \) |  sudo xargs -I {} \
+        iconv -c -t UTF-8 -o {} {}
+        # recode understands HTML entities!
+        # recode -v ..h4..utf-8 {}
+        # -c omits invalid characters from output
 }
 
 strip_comments() {
@@ -248,54 +276,77 @@ strip_comments() {
         -e 's|<!--.*-->||g';
 }
 
-#depends on YUI Compressor in build dir
-combine_js() {
-    NAME="scripts-$VERSION.min.css"
+# minify JS
+# filenames are provided as a list as a CLI argument or from conf file
+#combine_js() {
+#    NAME="scripts-$VERSION.min.css"
 
     #combine the named files
-    for jsfile in $JSFILES; do
-        cat $jsfile >> $NAME
-        rm -f $jsfile
-    end
+#    for jsfile in $JSFILES; do
+#        cat $jsfile >> $NAME
+#        rm -f $jsfile
+#    end
+#}
 
-}
-
-combine_css() {
-    NAME="styles-$VERSION.min.css"
+#combine_css() {
+#    NAME="styles-$VERSION.min.css"
 
     #combine the named files
-    for cssfile in $CSSFILES; do
-        cat $cssfile >> $NAME
+#    for cssfile in $CSSFILES; do
+#        cat $cssfile >> $NAME
 
         # delete the lines in html with this css file and replace with $name
-        find . -type f -name "*.html" | xargs sed -i \
-            -e "$cssfile d"
-        &&
-        rm -f $cssfile
-    end
+#        find . -type f -name "*.html" | xargs sed -i \
+#            -e "$cssfile d"
+#        &&
+#        rm -f $cssfile
+#    end
 
     # now add in the new $NAME CSS link
-    find . -type f -name "*.html" | xargs sed -i \
-"
-<!--[if IE]> i\
-<link rel=\"stylesheet\" type=\"text/css\" href=\"/css/lib/$NAME\" media=\"screen\" \/>
-"
+#    find . -type f -name "*.html" | xargs sed -i \
+#        "
+#        <!--[if IE]> i\
+#        <link rel=\"stylesheet\" type=\"text/css\" href=\"/css/lib/$NAME\" media=\"screen\" \/>
+#        "
+
+#        "
+#        <link rel=\"stylesheet\" type=\"text/css\" href=\"/css/lib/$NAME\" media=\"screen\" \/> d\
+#        "
+#}
+
+# --charset UTF-8
+# Remove extra spaces and comments from HTML
+minify_html() {
+    echo "Stripping coments, including Dreamweaver Template commands"
+    find . -type f -name "*.html" | sudo xargs -I {} \
+        java -jar $TO/htmlcompressor-0.9.3.jar --type html -o {} {}
+        # --remove-intertag-spaces  --remove-quotes (mgatto: "yuck!")
 }
-
-minify() {
-    find . -type f \( -name "*.js" -a -not name "jquery*" \) | sudo xargs -I {} \
-        java -jar $TO/build/yuicompressor-2.4.2.jar --type js {} -o {} --charset utf-8  --line-break 0
-
+minify_css() {
+    echo "Minifying CSS files"
     find . -type f -name "*.css" | sudo xargs -I {} \
-        java -jar $TO/build/yuicompressor-2.4.2.jar --type css {} -o {} --charset utf-8  --line-break 0
+        java -jar $TO/yuicompressor-2.4.2.jar --type css {} -o {} --charset utf-8  --line-break 0
+}
+minify_js() {
+    echo "Minifying Javascript files"
+    find . -type f \( -name "*.js" -a -not name "jquery*" \) | sudo xargs -I {} \
+        java -jar $TO/yuicompressor-2.4.2.jar --type js {} -o {} --charset utf-8  --line-break 0
 }
 
 # depends on pngcrush
 optimize_img() {
-    pngcrush
-    jpegtran -optimize -progressive < original.jpg > optimized.jpg
+    find . -type f -name "*.png" | sudo xargs -I {} \
+        optipng -o6
+
+
+    find . -type f -name "*.jpg" | sudo xargs -I {} \
+        jpegtran -optimize -progressive -copy none < original.jpg > optimized.jpg
+        # try not to strip copyright data from the image -copy = all
 
 }
+
+#error() {}
+#version() {}
 
 usage()
 {
@@ -303,7 +354,7 @@ usage()
         Usage: $PROGNAME [options]
                $PROGNAME -o <version> -c
 
-        Increase the .deb file's version number, noting the change in the
+        Deploy a static HTML website, and optionally optimize it.
 
 
         Options:
@@ -357,13 +408,22 @@ if [ -d "$VERSION" ]; then
 fi
 
 # Get the web files from a source
-if [ $ZIP -eq 1 -a $ARCHIVE != "" ]; then
+if [ $ZIP -eq 1 ]; then
+    if [ $ARCHIVE != "" ]; then
         get_from_zip $ARCHIVE
-elif [ $SVN -eq 1 -a $REPOSITORY != ""]; then
-        get_from_svn $REPOSITORY
-exit 0
+    else
+        echo "No archive for unzip was specified"
+    fi
+elif [ $SVN -eq 1 ]; then
+    if [ $REPOSITORY != "" ]; then
+        get_from_svn $REPOSITORY $TO $VERSION
+    else
+        echo "No repository for SVN was specified"
+        exit 1
+    fi
 else
-    echo "I don't know what to do next."
+    echo "Could not find a source!"
+    exit 1
 fi
 
 # Shall we rewrite links and src?
@@ -373,13 +433,16 @@ fi
 
 # Shall we minify js or css, or both? Images, too?
 # This should always come after Rewriting of links, if any.
-if [ $COMPRESS != "no" ]; then
-    compress $COMPRESS
+if [ $COMBINE != "no" ]; then
+    combine $COMBINE
 fi
 
 cleanup_files
-strip_comments
-minify
+# strip_comments
+minify_js
+minify_css
+minify_html
+
 # symlink must come before set_owner_and_group so the htdocs symlink ist't stuck as owned by root
 symlink
 set_owner_and_group www-data www-data
